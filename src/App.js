fa57d2c9e31f0582e5c4dd556b1210e9e806d3ce
Bryan Ko <bryankko1@gmail.com>
2020-01-25 16:33:10 -0800

    parallax position working

--- src/App.js
+++ src/App.js
@@ -1,34 +1,77 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { ParallaxProvider } from 'react-scroll-parallax';
-import IntroParallax from './components/IntroParallax/IntroParallax';
+import IntroBanner from './components/IntroBanner/IntroBanner';
+import IntroImg from './img/ucscBackground.jpg';
+
+import { Parallax, ParallaxProvider } from 'react-scroll-parallax';
+
+const HeroBanner = ({ image, min, max, children }) => (
+  <div className="hero-container">
+      <Parallax offsetYMin={min} offsetYMax={max} slowerScrollRate>
+          <div
+              className="hero-image"
+              style={{ backgroundImage: `url(${image})` }}
+          />
+      </Parallax>
+      <div className="hero-children">{children}</div>
+  </div>
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    //   </div>
+
+    // <div>
+    //   <ParallaxProvider>
+    //     <main>
+    //       <nav />
+    //       <IntroBanner
+    //         image={IntroImg}
+    //         min={"-20"}
+    //         max={"20"}
+    //       >
+    //       <h1>@</h1>
+    //       <h1>@</h1>
+    //       <h1>@</h1>
+    //       <h1>@</h1>
+    //       <h1>@</h1>
+    //       </IntroBanner>
+    //     </main>
+    //   </ParallaxProvider>
+    //   <div background-color="white" postion="absolute" margin-top="-50%">
+    //     Testing
+    //   </div>
+    // </div>
+
+    <ParallaxProvider>
+      <main>
+        <nav />
+        <IntroBanner
+          min={'-20%'}
+          max={'20%'}
+          image={IntroImg}
         >
-          Learn React
-        </a>
-      </header>
-      <ParallaxProvider
-        style={{
-          height: '500px',
-        }}
-          >
-          <IntroParallax />
-      </ParallaxProvider>
-    </div>
+          <h1>Hero Banner with Parallax</h1>
+        </IntroBanner>
+        <h1>Talk about me!</h1>
+      </main>
+    </ParallaxProvider>
   );
 }
 
---
+++ src/components/IntroBanner/IntroBanner.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './IntroBanner.css';
+
+import { Parallax } from 'react-scroll-parallax';
+
+const IntroBanner = ({ image, min, max, children }) => (
+	<div className="intro-container">
+		<Parallax y={[min, max]}>
+			<div
+				className="parallax-img"
+				style={{ backgroundImage: `url(${image})`  }}
+			/>
+			{/* <div className="intro-children">{children}</div> */}
+		</Parallax>
+		<div className="intro-children">{children}</div>
+	</div>
+);
+
+export default IntroBanner;
